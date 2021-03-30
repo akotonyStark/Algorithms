47148768fbbffa1966cd1c91fc4eff1b40e681eb
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-03-30 13:35:24 +0000

    included pop operation

--- Stack.java
+++ Stack.java
@@ -20,5 +20,14 @@ public class Stack{
         }
 
     }
+    
+    public String pop(){
+        String item = s[--N];
+        s[N] = null;
+        if(N > 0 && N == s.length/4){
+            resize(s.length/2);
+        }
+        return item;
+    }
 
 } 
