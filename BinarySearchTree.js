30f222188e979d1b68de5114a9ca9347239410f6
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-04 17:37:29 +0000

    Preorder traversal

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -18,97 +18,109 @@ class BinarySearchTree
         this.root = null;
     }
   
-// helper method which creates a new node to
-// be inserted and calls insertNode
-insert(data)
-{
-	// Creating a node and initailising
-	// with data
-	var newNode = new Node(data);
-					
-	// root is null then node will
-	// be added to the tree and made root.
-	if(this.root === null)
-		this.root = newNode;
-	else
-
-		// find the correct position in the
-		// tree and add the node
-		this.insertNode(this.root, newNode);
-}
-
-// Method to insert a node in a tree
-// it moves over the tree to find the location
-// to insert a node with a given data
-insertNode(node, newNode)
-{
-	// if the data is less than the node
-	// data move left of the tree
-	if(newNode.data < node.data)
+	// helper method which creates a new node to
+	// be inserted and calls insertNode
+	insert(data)
 	{
-		// if left is null insert node here
-		if(node.left === null)
-			node.left = newNode;
+		// Creating a node and initailising
+		// with data
+		var newNode = new Node(data);
+
+		// root is null then node will
+		// be added to the tree and made root.
+		if(this.root === null)
+			this.root = newNode;
 		else
 
-			// if left is not null recur until
-			// null is found
-			this.insertNode(node.left, newNode);
+			// find the correct position in the
+			// tree and add the node
+			this.insertNode(this.root, newNode);
 	}
 
-	// if the data is more than the node
-	// data move right of the tree
-	else
+	// Method to insert a node in a tree
+	// it moves over the tree to find the location
+	// to insert a node with a given data
+	insertNode(node, newNode)
 	{
-		// if right is null insert node here
-		if(node.right === null)
-			node.right = newNode;
-		else
+		// if the data is less than the node
+		// data move left of the tree
+		if(newNode.data < node.data)
+		{
+			// if left is null insert node here
+			if(node.left === null)
+				node.left = newNode;
+			else
 
-			// if right is not null recur until
-			// null is found
-			this.insertNode(node.right,newNode);
+				// if left is not null recur until
+				// null is found
+				this.insertNode(node.left, newNode);
+		}
+
+		// if the data is more than the node
+		// data move right of the tree
+		else
+		{
+			// if right is null insert node here
+			if(node.right === null)
+				node.right = newNode;
+			else
+
+				// if right is not null recur until
+				// null is found
+				this.insertNode(node.right,newNode);
+		}
 	}
-}
 	
-//finding a specific node
+	//finding a specific node
 	contains(value){
-  if(value === data){
-    return(true);
-  }
-  else if(value < data){
-    if(this.left == null){
-      return false;
-    }
-    else{
-      return this.left.contains(value)
-    }
-  }
-  else{
-    if(this.right == null){
-      return false
-    }
-    else{
-      return this.right.contains(value);
-    }
-  }
-}
-	
+	  if(value === data){
+	    return(true);
+	  }
+	  else if(value < data){
+	    if(this.left == null){
+	      return false;
+	    }
+	    else{
+	      return this.left.contains(value)
+	    }
+	  }
+	  else{
+	    if(this.right == null){
+	      return false
+	    }
+	    else{
+	      return this.right.contains(value);
+	    }
+	  }
+	}
+
 	
 	// Performs inorder traversal of a tree
-printInorder(node)
-{
-	if(node !== null)
+	printInorder(node)
 	{
-		this.printInorder(node.left);
-		console.log(node.data);
-		this.printInorder(node.right);
+		if(node !== null)
+		{
+			this.printInorder(node.left);
+			console.log(node.data);
+			this.printInorder(node.right);
+		}
 	}
-}
+	
+	// Performs preorder traversal of a tree	
+	printPreorder(node)
+	{
+		if(node !== null)
+		{
+			console.log(node.data);
+			this.printPreorder(node.left);
+			this.printPreorder(node.right);
+		}
+	}
+
 
 // Helper function
     // remove(data)               
-    //insertNode(node, newNode)
+    // insertNode(node, newNode)
     // contains(node)
     // findMinNode()
     // getRootNode()
