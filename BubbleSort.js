717648624f82b9faef508508c5e05f649b2ed7ff
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-07-30 20:45:05 +0000

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,3 +1,19 @@
+/* optimal solution */
+const arr = [37,45,29,8]
+
+for(let i=arr.length; i>0; i--){
+  for(let j=0; j<i-1; j++){
+    console.log(arr, arr[j], arr[j+1])
+    if(arr[j] > arr[j+1]){
+      let temp = arr[j]
+      arr[j] = arr[j+1]
+      arr[j+1] = temp
+    }
+  }
+}
+
+console.log(arr)
+
 /* solution one */
 
 let array = [2,1,5,-1,7,8,14,0,9]
